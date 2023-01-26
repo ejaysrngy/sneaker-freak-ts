401951ef717e6ba069677db29423a007d3d6f5c0
ejaysrngy <ejaysarangay@gmail.com>
2023-01-26 18:00:06 +0800

    Add footer component; install axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import HeroBanner from './components/HeroBanner';
+import Footer from './components/Footer/Footer';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <HeroBanner />
+      <main id="main-section">
+        {/* break off this section to a component
+            pass in children in along with HeroBanner
+        */}
+        <HeroBanner />
+      </main>
+      <Footer />
     </div>
   );
 }
