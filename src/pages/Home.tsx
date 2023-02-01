c5be5281caec182eea7f74a78411d51de57e5d97
ejaysrngy <ejaysarangay@gmail.com>
2023-02-01 21:07:08 +0800

    Add router; config tsconfig

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from 'components/Layout'
+import Body from 'components/Body/Body'
+
+const HomePage = () => {
+    return (
+        <Layout>
+            <Body/>
+        </Layout>
+    )
+}
+
+export default HomePage
